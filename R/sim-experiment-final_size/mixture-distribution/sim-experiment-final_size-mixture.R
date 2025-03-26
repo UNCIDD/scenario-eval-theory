83701fadbd9f54a5de1b647c012c6e04204c6fd5
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2025-03-26 11:34:27 -0400

    create function to run full analysis

--- R/sim-experiment-final_size/mixture-distribution/sim-experiment-final_size-mixture.R
+++ R/sim-experiment-final_size/mixture-distribution/sim-experiment-final_size-mixture.R
@@ -153,7 +153,7 @@ fit_one_boxcox <- function(error_df, new_vax_cov, alphas, neg_flag = FALSE){
 # to do: add plot flag if interested
 fit_mixture_w_boxcox <- function(model_errors, new_vax_cov, alphas,
                                  plot_flag = FALSE, plot_title = NA,
-                                 n_draws = 1e5, fit_thresh = 10){ 
+                                 n_draws, fit_thresh){ 
     neg_errors <- model_errors %>% filter(error < 0)
     pos_errors <- model_errors %>% filter(error > 0)
     prop_neg = nrow(neg_errors)/nrow(model_errors)
@@ -248,115 +248,97 @@ plot_ind_boxcox_fits <- function(error, pred_nominal_pos, pred_nominal_neg,
   print(p)
 }
 
-#### RUN SOME SIMULATIONS ------------------------------------------------------
-preds_all <- vector("list", length = n_models)
-for(i in 1:n_models){
-  print(i)
-  dat_filt = error_df %>% filter(model_id == paste0("M", i))
-  preds_all[[i]] = fit_mixture_w_boxcox(
-    dat_filt, new_vax_cov, alphas, plot_flag = TRUE, plot_title = paste("Model:", i)) %>% 
-    mutate(model_id = paste0("M", i))
+run_full_analysis <- function(all_errors, new_vax_cov, alphas, n_draws = 1e5, fit_thresh = 10,
+                              plot_ind_boxcox_fits_flag = FALSE, calculate_coverage_flag = TRUE, 
+                              plot_full_results_flag = TRUE, full_results_title = NA){
+  # subset to only observed errors (i.e., scenario_id = T returns only observed errors)
+  observered_errors = all_errors %>% filter(scenario_id == "T")
+  # subset to only true errors to test against (i.e., scenario_id == "S1", "S2")
+  true_errors = all_errors %>% filter(scenario_id %in% c("S1", "S2"))
+  # create container to store results
+  full_results_list <- list()
+  # fit mixture distribution for each mdoel
+  preds_all <- vector("list", length = n_models)
+  for(i in 1:n_models){
+    print(i)
+    dat_filt = observered_errors %>% filter(model_id == paste0("M", i))
+    preds_all[[i]] = fit_mixture_w_boxcox(
+      dat_filt, new_vax_cov, alphas, plot_flag = plot_ind_boxcox_fits_flag, 
+      plot_title = paste("Model:", i)) %>% 
+      mutate(model_id = paste0("M", i))
+  }
+  full_results_list$prediction = bind_rows(preds_all)
+  # calculate coverage
+  if(calculate_coverage_flag){
+    cov = calculate_coverage(
+      # do some reshaping to match expected format for quant_fits
+      quant_fits = bind_rows(preds_all) %>% 
+        melt(c("model_id", "vax_cov")) %>% 
+        filter(variable != "mean") %>%
+        mutate(quantile = as.integer(gsub("Q", "", variable))/1000) %>%
+        dplyr::select(-variable), 
+      error_df = true_errors,  # %>% mutate(error = abs(error))
+      vax_cov_S1 = 0.3, vax_cov_S2 = 0.5, 
+      summarize_by = "model"
+    )
+    full_results_list$coverage = cov
+  }
+  # plot results
+  if(plot_full_results_flag){
+    p1 = bind_rows(preds_all) %>% 
+      ggplot(aes(x = vax_cov)) + 
+      # geom_point(data = t_large$errors %>% filter(scenario_id %in% c("S1", "S2")),
+      #            aes(y = error), color = "darkgray", shape = 8) +
+      # geom_line(data = t_large$errors %>% filter(scenario_id == "E"),
+      #           aes(y = error, group = location_id), color = "darkgray", alpha = 0.25) +
+      geom_point(data = observered_errors, aes(y = error), shape = 21) +
+      geom_ribbon(aes(ymin = Q25, ymax = Q975, fill = model_id), alpha = 0.4) +
+      geom_ribbon(aes(ymin = Q250, ymax = Q750, fill = model_id), alpha = 0.6) +
+      geom_line(aes(y = Q500, color = model_id), size = 1) + 
+      facet_wrap(vars(model_id)) + 
+      labs(x = "vaccination coverage", y = "absolute error", 
+           title = full_results_title) + 
+      theme_bw() + 
+      theme(legend.position = "none")
+    p2 = ggplot(data = cov, aes(x = alpha, y = cov, group = model_id)) + 
+      geom_line(aes(color = model_id)) + 
+      geom_abline(size = 1) + 
+      facet_wrap(vars(scenario_id), labeller = labeller(scenario_id = scenario_labs), ncol = 1) + 
+      labs(x = "expected coverage", y = "actual coverage") + 
+      theme_bw() + 
+      theme(legend.position = "none")
+    p = cowplot::plot_grid(p1, p2, rel_widths = c(0.7, 0.3), align = "v", axis = "t") 
+    full_results_list$p = p
+  }
+  return(full_results_list)
 }
+  
 
-p1 = bind_rows(preds_all) %>% 
-  ggplot(aes(x = vax_cov)) + 
-  # geom_point(data = t_large$errors %>% filter(scenario_id %in% c("S1", "S2")),
-  #            aes(y = error), color = "darkgray", shape = 8) +
-  # geom_line(data = t_large$errors %>% filter(scenario_id == "E"),
-  #           aes(y = error, group = location_id), color = "darkgray", alpha = 0.25) +
-  geom_point(data = error_df, aes(y = error), shape = 21) +
-  geom_ribbon(aes(ymin = Q25, ymax = Q975, fill = model_id), alpha = 0.4) +
-  geom_ribbon(aes(ymin = Q250, ymax = Q750, fill = model_id), alpha = 0.6) +
-  geom_line(aes(y = Q500, color = model_id), size = 1) + 
-  facet_wrap(vars(model_id)) + 
-  labs(x = "vaccination coverage", y = "absolute error") + 
-  theme_bw() + 
-  theme(legend.position = "none")
-p1
-
-# find coverage and plot
-fits_var_small = calculate_coverage(
-  # do some reshaping to match expected format for quant_fits
-  quant_fits = bind_rows(preds_all) %>% 
-    melt(c("model_id", "vax_cov")) %>% 
-    filter(variable != "mean") %>%
-    mutate(quantile = as.integer(gsub("Q", "", variable))/1000) %>%
-    dplyr::select(-variable), 
-  error_df = t_large$errors,  # %>% mutate(error = abs(error))
-  vax_cov_S1 = 0.3, vax_cov_S2 = 0.5, 
-  summarize_by = "model"
+#### TRY THE FULL ANALYSIS -----------------------------------------------------
+r_large <- run_full_analysis(
+  all_errors = t_large$errors, new_vax_cov = new_vax_cov, alphas = alphas, 
+  plot_ind_boxcox_fits_flag = FALSE, calculate_coverage_flag = TRUE, 
+  plot_full_results_flag = TRUE, 
+  full_results_title = "500 locations, low correlation between R0 and true vax (0.05)"
 )
 
-p2 = ggplot(data = fits_var_small, aes(x = alpha, y = cov, group = model_id)) + 
-  geom_line(aes(color = model_id), size = 1, alpha = 0.7) + 
-  geom_abline(size = 1) + 
-  facet_wrap(vars(scenario_id), labeller = labeller(scenario_id = scenario_labs), ncol = 1) + 
-  labs(x = "expected coverage", y = "actual coverage") + 
-  theme_bw() + 
-  theme(legend.position = "none")
-p2
-
-cowplot::plot_grid(p1, p2, rel_widths = c(0.7, 0.3))
+r_large$p
 ggsave("R/sim-experiment-final_size/mixture-distribution/500locations_lowcorr.pdf", width = 14, height = 6)
 
 #### REPEAT WITH FEWER LOCATIONS -----------------------------------------------
-# run simulation - now using 50 locations
 t_small <- full_sim(n_locations = 50, n_models = n_models,
                     vax_cov_S1 = 0.3, vax_cov_S2 = 0.5,
                     R0_lwr = 2, R0_upr = 3,
                     model_bias_ind_sd = 0.05,
                     seed = seed_id, fit_outcomes = FALSE)
 
-# note: scenario_id = T returns only observed errors (not true errors to test against)
-error_df_small = t_small$errors %>% filter(scenario_id == "T") 
 
-preds_all_small <- vector("list", length = n_models)
-for(i in 1:n_models){
-  print(i)
-  dat_filt = error_df_small %>% filter(model_id == paste0("M", i))
-  preds_all_small[[i]] = fit_mixture_w_boxcox(
-    dat_filt, new_vax_cov, alphas, plot_flag = TRUE, plot_title = paste("Model:", i)) %>% 
-    mutate(model_id = paste0("M", i))
-}
-
-p1 = bind_rows(preds_all_small) %>% 
-  ggplot(aes(x = vax_cov)) + 
-  # geom_point(data = t_large$errors %>% filter(scenario_id %in% c("S1", "S2")),
-  #            aes(y = error), color = "darkgray", shape = 8) +
-  # geom_line(data = t_large$errors %>% filter(scenario_id == "E"),
-  #           aes(y = error, group = location_id), color = "darkgray", alpha = 0.25) +
-  geom_point(data = error_df_small, aes(y = error), shape = 21) +
-  geom_ribbon(aes(ymin = Q25, ymax = Q975, fill = model_id), alpha = 0.4) +
-  geom_ribbon(aes(ymin = Q250, ymax = Q750, fill = model_id), alpha = 0.6) +
-  geom_line(aes(y = Q500, color = model_id), size = 1) + 
-  facet_wrap(vars(model_id)) + 
-  labs(x = "vaccination coverage", y = "absolute error") + 
-  theme_bw() + 
-  theme(legend.position = "none")
-p1
-
-# find coverage and plot
-fits_var_small = calculate_coverage(
-  # do some reshaping to match expected format for quant_fits
-  quant_fits = bind_rows(preds_all_small) %>% 
-    melt(c("model_id", "vax_cov")) %>% 
-    filter(variable != "mean") %>%
-    mutate(quantile = as.integer(gsub("Q", "", variable))/1000) %>%
-    dplyr::select(-variable), 
-  error_df = t_small$errors,  # %>% mutate(error = abs(error))
-  vax_cov_S1 = 0.3, vax_cov_S2 = 0.5, 
-  summarize_by = "model"
+r_small <- run_full_analysis(
+  all_errors = t_small$errors, new_vax_cov = new_vax_cov, alphas = alphas, 
+  plot_ind_boxcox_fits_flag = FALSE, calculate_coverage_flag = TRUE, 
+  plot_full_results_flag = TRUE, 
+  full_results_title = "50 locations, low correlation between R0 and true vax (0.05)"
 )
 
-p2 = ggplot(data = fits_var_small, aes(x = alpha, y = cov, group = model_id)) + 
-  geom_line(aes(color = model_id), size = 1, alpha = 0.7) + 
-  geom_abline() + 
-  facet_wrap(vars(scenario_id), labeller = labeller(scenario_id = scenario_labs), ncol = 1) + 
-  labs(x = "expected coverage", y = "actual coverage") + 
-  theme_bw() + 
-  theme(legend.position = "none")
-p2
-
-cowplot::plot_grid(p1, p2, rel_widths = c(0.65, 0.35))
+r_small$p
 ggsave("R/sim-experiment-final_size/mixture-distribution/50locations_lowcorr.pdf", width = 14, height = 6)
-
