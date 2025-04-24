19175ddd35509dbecb20fea76b380aedf8a748c1
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2025-04-24 16:57:35 -0400

    Add overview figure

--- R/sim-experiment-final_size/mixture-distribution/fit-mixture-model-stan.R
+++ R/sim-experiment-final_size/mixture-distribution/fit-mixture-model-stan.R
@@ -311,8 +311,7 @@ t_small <- full_sim(n_locations = 50, n_models = n_models,
                     seed = seed_id, fit_outcomes = FALSE)
 
 # note: scenario_id = T returns only observed errors (not true errors to test against)
-error_df_small = t_small$errors %>% filter(scenario_id == "T") 
-          
+error_df_small = t_small$errors %>% filter(scenario_id == "T")
 
 #### FIT WITH SHRINKAGE PARAMETERS ---------------------------------------------
 fit_errors_shrinkage_small <- vector("list", n_models)
@@ -382,4 +381,73 @@ ggplot(data = cov_shrink_small, aes(x = alpha, y = cov, group = model_id)) +
   labs(x = "expected coverage", y = "actual coverage") +
   theme_bw() +
   theme(legend.position = "none")
+
+#### MAKE A FIGURE TO ILLUSTRATE THE APPROACH ----------------------------------
+locs_to_plot = c(29, 5, 45)
+loc_labs = c(15, 41, 32)
+loc_cols = RColorBrewer::brewer.pal(4, "Set1")[2:4]
+model_to_plot = "M4"
+
+# overall results
+mod_results = ggplot(data = pred_intervals_shrink_small %>% filter(model_id == model_to_plot), 
+                    aes(x = vax_cov)) +
+  geom_point(data = error_df_small %>% filter(model_id == model_to_plot), 
+             aes(x = vax_cov, y = error), color = "black", shape = 21, size = 2.5) +
+  geom_ribbon(aes(ymin = Q5, ymax = Q95, fill = model_id), alpha = 0.15, fill = "darkgray") +
+  geom_ribbon(aes(ymin = Q25, ymax = Q75, fill = model_id), alpha = 0.3, fill = "darkgray") +
+  geom_line(aes(y = Q50, color = model_id), alpha = 0.8, size = 1.5, color = "darkgray") +
+  geom_point(data = error_df_small %>% filter(model_id == model_to_plot, location_id %in% locs_to_plot), 
+            aes(y = error, color = as.factor(location_id)), size = 2.5) + 
+  geom_text(data = error_df_small%>% 
+              filter(model_id == model_to_plot, location_id %in% locs_to_plot) %>% 
+              mutate(loc_lab = loc_labs[which(locs_to_plot == location_id)], .by = "location_id"),
+            aes(y = error, label = loc_lab), size = 2, color = "white") +
+  facet_wrap(vars(model_id), scales = "free") +
+  labs(x = "realized vaccine uptake\n(scenario axis)") +
+  scale_color_manual(values = loc_cols) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        panel.grid.minor =  element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.background = element_blank(), 
+        strip.text = element_blank())
+mod_results
+
+# a few specific locations
+loc_results = ggplot(data = t_small$model_sims %>% 
+                       filter(model_id %in% c(model_to_plot, "T"), location_id %in% locs_to_plot, 
+                              scenario_id != "E") %>%
+                       mutate(location_id = factor(location_id, levels = locs_to_plot))) + 
+  geom_line(data = t_small$model_sims %>% 
+              filter(model_id == model_to_plot, location_id %in% locs_to_plot) %>%
+              mutate(location_id = factor(location_id, levels = locs_to_plot)), 
+             aes(x = vax_cov, y = final_size), alpha = 0.2) + 
+  geom_point(aes(x = vax_cov, y = final_size, shape = scenario_id, fill = scenario_id), color = 'black', size = 3) +
+  geom_text(data = data.frame(location_id = locs_to_plot, 
+                              location_lab = loc_labs) %>%
+              mutate(location_id = factor(location_id, levels = locs_to_plot)),
+            aes(x = Inf, y = Inf, label = paste0("location ", location_lab), color = as.factor(location_id)), 
+            hjust = 1, vjust = 1, size = 5) +
+  geom_point(data = t_small$true_sims %>% filter(location_id %in% locs_to_plot, scenario_id == "T"), 
+             aes(x = vax_cov, y = true_final_size), color = "red", size = 3) + 
+  geom_segment(data = left_join(
+    t_small$model_sims %>% filter(model_id == model_to_plot, location_id %in% locs_to_plot, scenario_id == "T"),
+    t_small$true_sims %>% filter(location_id %in% locs_to_plot, scenario_id == "T")
+  ), aes(x = vax_cov, xend = vax_cov, y = true_final_size, yend = final_size), arrow = arrow(length = unit(0.03, "npc"))) +
+  facet_wrap(vars(location_id), ncol = 1) +
+  labs(x = "realized vaccine uptake\n(scenario axis)", 
+       y = "cumulative hospitalizations\n(projection axis)") +
+  scale_color_manual(values = loc_cols[sapply(sort(locs_to_plot), function(i){which(locs_to_plot == i)})]) +
+  scale_fill_manual(values = c("black", "black", "white")) +
+  scale_shape_manual(values = c(16, 16, 21)) +
+  theme_bw() +
+  theme(legend.position = "none", 
+        panel.grid = element_blank(), 
+        strip.background = element_blank(), 
+        strip.text = element_blank())
+loc_results
+
+cowplot::plot_grid(loc_results, mod_results, rel_widths = c(0.3, 0.7))
+
+ggsave("R/sim-experiment-final_size/mixture-distribution/approach_illustration.pdf", width = 7, height = 3.5)
 
