63dcf3b8c177e85e5c42be40baa4faa764de1112
Emily Howerton <46577370+eahowerton@users.noreply.github.com>
2025-11-06 15:43:24 -0500

    Update 0-parameters.R

--- R/simulation/0-parameters.R
+++ R/simulation/0-parameters.R
@@ -16,4 +16,11 @@ quantiles = c(0.001, 0.01, 0.025, seq(0.05, 0.95, 0.05), 0.975, 0.99, 0.999)
 alphas = sort(unique(sapply(quantiles, function(i){round(ifelse(i < 0.5, 1-2*i, 1-2*(1-i)),3)})))
 # alphas = alphas[-which(alphas == 0)]
 
+# plotting utilities
+approach_labs = c("truth", "approach 1: most plausible scenario", 
+                  "approach 2: estimate observations (no covariates)", "approach 2: estimate observations (covariates)", 
+                  "approach 3: estimate error (no covariates)", "approach 3: estimate error (covariates)")
+names(approach_labs) = c("truth", "1", "2-nocovariates", "2-covariates", "3-nocovariates", "3-covariates")
 
+model_labs = paste0("model ", 1:n_models)
+names(model_labs) = paste0("M", 1:n_models)
